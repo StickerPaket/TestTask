12a832f0e8fb96dce8cb9baaed689c81984a5655
krasnalex <elfbrech@gmail.com>
2023-05-05 11:06:02 +0400

    Добавьте файлы проекта.

---
+++ TestTask/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
+using TestTask.Models;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace TestTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult SaveHuman() => View();
+
+        [HttpPost]
+        public IActionResult SaveHuman(HumanModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            SaveDataToJson(model);
+
+            return RedirectToAction("Saved");
+        }
+
+        private void SaveDataToJson(HumanModel model)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string json = JsonSerializer.Serialize(model, options);
+            string directory = Directory.GetCurrentDirectory() + "\\Data\\Humans\\";
+            string fileExtension = ".json";
+            string fileName = "human";
+
+            int i = 1;
+            while (System.IO.File.Exists(directory + fileName + i + fileExtension))
+            {
+                i++;
+            }
+            string path = directory + fileName + i + fileExtension;
+
+            StreamWriter sw = new StreamWriter(path);
+            sw.Write(json);
+            sw.Close();
+        }
+        public IActionResult Saved() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestTask/Models/HumanModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestTask.Models
+{
+    public class HumanModel
+    {
+        [Required(ErrorMessage = "Поле \"Фамилия\" обязательно для ввода")]
+        [MinLength(2, ErrorMessage = "Поле \"Фамилия\" должно быть не короче 2 символов")]
+        [MaxLength(50, ErrorMessage = "Поле \"Фамилия\" должно быть не длиннее 50 символов")]
+        [RegularExpression("^[a-zA-Zа-яА-я]+", ErrorMessage = "В Поле \"Фамилия\" не должно быть специальных символов")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Имя\" обязательно для ввода")]
+		[MinLength(2, ErrorMessage = "Поле \"Имя\" должно быть не короче 2 символов")]
+        [MaxLength(50, ErrorMessage = "Поле \"Имя\" должно быть не длиннее 50 символов")]
+        [RegularExpression("^[a-zA-Zа-яА-я]+", ErrorMessage = "В Поле \"Имя\" не должно быть специальных символов")]
+        public string Name { get; set; }
+
+		[MinLength(2, ErrorMessage = "Поле \"Отчество\" должно быть не короче 2 символов")]
+        [MaxLength(50, ErrorMessage = "Поле \"Отчество\" должно быть не длиннее 50 символов")]
+        [RegularExpression("^[a-zA-Zа-яА-я]+", ErrorMessage = "В Поле \"Отчество\" не должно быть специальных символов")]
+        public string? Patronymic { get; set; } = null;
+
+        [Required(ErrorMessage = "Поле \"Дата рождения\" обязательно для ввода")]
+        [RegularExpression("[0-9]{4}-[0-9]{2}-[0-9]{2}", ErrorMessage = "В Поле \"Фамилия\" не должно быть специальных символов")]
+        public string DateBirth { get; set; }
+    }
+}
